6da7c6288e2a8cc593c1c77270cbb1fe68e8c8d2
JSaidi1 <jabrane_saidi@outlook.com>
2025-12-01 15:21:06 +0100

    feature(database): 2. 3. 4.

--- exercice_1/sql/requests.sql
+++ exercice_1/sql/requests.sql
@@ -16,3 +16,43 @@ SELECT
 FROM sql_avancee_exercice_1_schema.tracks as t
 JOIN sql_avancee_exercice_1_schema.artists as a
 	ON t.artist_id = a.artist_id;
+
+/*
+2. Utilisateurs Premium français
+L’équipe marketing souhaite travailler spécifiquement sur les utilisateurs :
+
+ayant un abonnement Premium,--users(subscription)
+résidant en France. --users(country)
+
+Créer une vue filtrée permettant d’identifier ces utilisateurs, puis l’utiliser pour obtenir une liste ordonnée.
+*/
+CREATE OR REPLACE VIEW sql_avancee_exercice_1_schema.v_users_premium AS
+SELECT
+    u.user_id,
+	u.subscription,
+	u.country,
+	l.track_id, 
+	l.listened_at, 
+	l.seconds_played
+FROM sql_avancee_exercice_1_schema.users u
+LEFT JOIN  sql_avancee_exercice_1_schema.listenings l
+	on u.user_id = l.user_id
+WHERE subscription = 'Premium'
+	AND country = 'France'
+
+/*
+4. Statistiques d’écoute par artiste
+Pour optimiser l’analyse, cette statistique doit être construite à partir d’une vue matérialisée reposant sur les écoutes détaillées :
+
+Pour chaque artiste, calculer :
+
+le nombre total d’écoutes,
+le nombre total de secondes écoutées,
+la durée moyenne écoutée par écoute.
+Créer cette vue matérialisée, puis l’utiliser pour identifier les artistes les plus écoutés selon différents critères (par exemple ceux qui ont un nombre d’écoutes élevé, ou un volume total de lecture important).
+*/
+CREATE MATERIALIZED VIEW mv_listenings_stats AS
+SELECT COUNT()
+FROM sql_avancee_exercice_1_schema.artists a
+	
+GROUP BY artist_id
